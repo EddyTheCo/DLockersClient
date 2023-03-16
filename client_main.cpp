ad570b437d1e19477d6f6e7cd81bd49cc0e2f92e
EddyGlezz <eddyglezz95@gmail.com>
2023-03-16 13:06:59 +0100

    look file from the resource files after the prefix

--- client_main.cpp
+++ client_main.cpp
@@ -3,23 +3,26 @@
 #include "Qrimageprovider.hpp"
 
 
-
 int main(int argc, char *argv[])
 {
 	QGuiApplication app(argc, argv);
 
 	QQmlApplicationEngine engine;
+	engine.addImportPath("qrc:/esterVtech.com/imports");
+    engine.addImportPath("_deps/mydesigns-build/");
+    engine.addImportPath("_deps/account-build/");
+    engine.addImportPath("_deps/nodeconection-build/");
+    engine.addImportPath("_deps/booking_model-build//");
 	engine.addImageProvider(QLatin1String("qrcodeblack"), new QRImageProvider("black",1));
+	qDebug()<<engine.importPathList();
 
-	const QUrl url(u"qrc:/binter/binter/qml/window_client.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                            &app, [url](QObject *obj, const QUrl &objUrl) {
-                            if (!obj && url == objUrl)
-                            QCoreApplication::exit(-1);
-                            }, Qt::QueuedConnection);
+    const QUrl url(u"qrc:/esterVtech.com/imports/client/qml/window.qml"_qs);
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+			&app, [url](QObject *obj, const QUrl &objUrl) {
+			if (!obj && url == objUrl)
+			QCoreApplication::exit(-1);
+			}, Qt::QueuedConnection);
 
 	engine.load(url);
-
-
 	return app.exec();
 }
