e7dacb9c1b4ca3c0969af2f9c0d0979bf1d4138f
EddyGlezz <eddyglezz95@gmail.com>
2023-05-18 13:27:05 +0200

    added support for funds and show the client address

--- include/bclient.hpp
+++ include/bclient.hpp
@@ -6,16 +6,16 @@
 #include<booking.hpp>
 #include<account.hpp>
 #include"nodeConnection.hpp"
-#include <queue>
+
 
 class Book_Client : public QObject
 {
     Q_OBJECT
 
     Q_PROPERTY(QString  server_id READ get_server_id WRITE set_server_id NOTIFY server_idChanged)
-    Q_PROPERTY(QString  code_str READ get_code_str WRITE set_code_str NOTIFY code_str_changed)
-    Q_PROPERTY(unsigned int  topay READ get_topay NOTIFY topay_changed)
-    Q_PROPERTY(ConState  state READ state NOTIFY stateChanged)
+    Q_PROPERTY(ConState  state READ state WRITE set_state NOTIFY stateChanged)
+    Q_PROPERTY(QString  clientId READ clientId NOTIFY clientIdChanged)
+    Q_PROPERTY(QJsonObject  funds READ funds  NOTIFY fundsChanged)
     QML_ELEMENT
     QML_SINGLETON
 
@@ -27,49 +27,47 @@ public:
         Connected
     };
     Q_ENUM(ConState)
-    Q_INVOKABLE void try_to_book(const std::vector<Booking> &books);
     Q_INVOKABLE void restart(){
         monitor_state();
     }
+    Q_INVOKABLE void send_booking(const QJsonArray books);
+    Q_INVOKABLE void presentNft(const QString address);
     void set_server_id(QString server_id_m);
     QString get_server_id(void)const;
-    QString get_code_str(void)const{return code_str_;}
-
-
-    quint64 get_topay(void)const{return topay_;}
-
-    void set_code_str(QString code_str_m){code_str_=code_str_m;emit code_str_changed();}
-
+    void sendNft(Node_output out,c_array address);
+    QString clientId(void)const{return clientId_;}
+    void setClientId(QString clie){if(clie!=clientId_){clientId_=clie;emit clientIdChanged();}}
+    QJsonObject funds(void)const{return funds_json;}
 
      static ConState state(void){return state_;}
      void set_state(ConState state_m){if(state_m!=state_){state_=state_m;emit stateChanged();}}
 signals:
-    void code_str_changed();
-    void topay_changed();
-    void sent_book(Booking);
-    void removed_expired(Booking);
-    void got_new_booking(Booking book);
+    void sent_book(QJsonArray books,QString outid);
+
+    void got_new_booking(QJsonArray books);
     void server_idChanged();
     void stateChanged();
-
+    void notEnought(QJsonObject);
+    void removed_expired(QString);
+    void clientIdChanged();
+    void fundsChanged();
 
 private:
-    void send_booking(Booking book);
-
-    void set_to_pay(quint64 price){if(price!=topay_){topay_=price;emit topay_changed();}}
+    void checkFunds(std::vector<qiota::Node_output>  outs);
+    void setFunds(quint64 funds_m);
     void monitor_state(void);
     void check_if_expired(void);
     void check_state_output(const Node_output &node_output_);
+    void deserialize_state(const QByteArray &state);
 
-
-
-
-    QString code_str_;
-    quint64 price_per_hour_,topay_;
+    quint64 price_per_hour_;
     std::vector<Booking> books_;
-    qblocks::c_array server_id_,paytoaddrhash_;
+    qblocks::c_array server_id_,paytoaddr_;
     QObject* reciever;
     static ConState state_;
-    std::queue<Booking> queue;
-
+    bool inited;
+    QString clientId_;
+    QJsonObject funds_json;
+    quint64 funds_;
+    QHash<QString,quint64> total_funds;
 };
