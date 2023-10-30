79d6440c95ea9b9aecee66dd42bbfd1430c03915
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-10-30 11:02:27 +0100

    Fixes (#11)
    * Use the latest dependencies and link to plugins in static
    
    * Use the latest api of the qml singleton types

--- client_main.cpp
+++ client_main.cpp
@@ -1,7 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "Qrimageprovider.hpp"
-#include"Day_model.hpp"
 #include"Qrimagedecoder.hpp"
 int main(int argc, char *argv[])
 {
@@ -11,19 +10,12 @@ int main(int argc, char *argv[])
 	QQmlApplicationEngine engine;
 	engine.addImportPath("qrc:/esterVtech.com/imports");
 	engine.addImageProvider(QLatin1String("qrcodeblack"), new QRImageProvider("black",1));
-#ifdef USE_EMSCRIPTEN
 	engine.addImageProvider(QLatin1String("wasm"), new WasmImageProvider());
-#endif
+
 	qDebug()<<engine.importPathList();
 
 	qmlRegisterSingletonType(QUrl(u"qrc:/esterVtech.com/imports/MyDesigns/qml/CustomStyle.qml"_qs), "CustomStyle", 1, 0, "CustomStyle");
 	const QUrl url(u"qrc:/esterVtech.com/imports/client/qml/window.qml"_qs);
-	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-			&app, [url](QObject *obj, const QUrl &objUrl) {
-			if (!obj && url == objUrl)
-			QCoreApplication::exit(-1);
-			}, Qt::QueuedConnection);
-
 	engine.load(url);
 	return app.exec();
 }
